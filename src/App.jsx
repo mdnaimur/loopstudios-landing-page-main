b55ce58b8e8846eac1982b9537a45840d4c768bd
mdnaimur <mdnaimurr@gmail.com>
2024-12-03 22:23:15 +0600

    react implement

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="text-center bg-slate-300 text-blue-500 text-xl">
+        <h1>Md Naimur Rahman</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
